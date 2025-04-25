677d4cb9d829b36389623cbb89604b7b75568e38
brain <brian950530@gmail.com>
2025-04-25 16:00:35 +0800

    用While迴圈寫出九九乘法表和直角三角形的算法

--- E1B31第二次作業.c
+++ E1B31第二次作業.c
@@ -2,6 +2,40 @@
 #include<stdlib.h>
 
 #define PASSWORD 2025
+
+int clearScreen() {
+    system("cls");  // 清除畫面（只適用於 Windows）
+    return 0;
+}
+int drawTriangle(char ch) {
+    int height = ch - 'a' + 1;
+    int i = 1;
+    while (i <= height) {
+        int j = 1;
+        while (j <= i) {
+            printf("%c", ch);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
+
+int multiplicationTable(int n) {
+    int i = 1;
+    while (i <= n) {
+        int j = 1;
+        while (j <= n) {
+            printf("%4d", i * j);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
+
 int main(){
 	printf("E1B31\n");
 	printf("E1B31\n");
@@ -23,6 +57,7 @@ int main(){
 	printf("E1B31\n");
 	printf("E1B31\n");
 	printf("E1B31\n");
+	
 	
 	return 0;
 } 
