d4ac3a4fcbc51bfe7e730c9f67a05616578c64e5
brain <brian950530@gmail.com>
2025-04-25 15:35:52 +0800

    寫出個人風格畫面，以及定義密碼

--- E1B31第二次作業.c
+++ E1B31第二次作業.c
@@ -1,8 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+#define PASSWORD 2025
 int main(){
-	
-	
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
 	
 	return 0;
 } 
