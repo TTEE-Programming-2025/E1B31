b6f9c42cf803533fbdc40d53d1c68591d7bb6d09
倪柏恩 <brian950530@gmail.com>
2025-05-20 22:55:16 +0800

    寫20行個人畫面

---
+++ p4E1B31.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	printf("E1B31\n");
+	
+	system("pause");
+	return 0;
+} 
