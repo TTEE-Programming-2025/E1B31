e6d7a3659d5e5584c23ccc6b88a11ec2600146d3
倪柏恩 <brian950530@gmail.com>
2025-05-20 23:22:34 +0800

    輸入成績的函數和列出成績的函數

--- p4E1B31.c
+++ p4E1B31.c
@@ -38,6 +38,61 @@ void welcomeScreen() {
     printf("錯誤次數過多，程式結束。\n");
     exit(0);
 }
+
+void showMenu() {
+    printf("--------- [Grade System] ---------\n");
+    printf("a. Enter student grades\n");
+    printf("b. Display student grades\n");
+    printf("c. Search for student grades\n");
+    printf("d. Grade ranking\n");
+    printf("e. Exit system\n");
+    printf("----------------------------------\n");
+    printf("請輸入選項: ");
+}
+
+void enterGrades() {
+    clearScreen();
+    printf("請輸入學生人數 (5~10): ");
+    scanf("%d", &student_count);
+    if (student_count < 5 || student_count > 10) {
+        printf("人數錯誤，請重新輸入！\n");
+        student_count = 0;
+        return;
+    }
+
+    for (int i = 0; i < student_count; i++) {
+        printf("\n第 %d 位學生:\n", i + 1);
+        printf("姓名: ");
+        scanf("%s", students[i].name);
+        printf("學號: ");
+        scanf("%s", students[i].id);
+        students[i].average = 0;
+        char *subjects[] = {"數學", "英文", "物理"};
+        for (int j = 0; j < SUBJECTS; j++) {
+            printf("%s 成績 (0~100): ", subjects[j]);
+            scanf("%d", &students[i].scores[j]);
+            students[i].average += students[i].scores[j];
+        }
+        students[i].average /= SUBJECTS;
+    }
+    printf("\n成績輸入完成，按任意鍵返回主選單。\n");
+    system("pause");
+}
+
+void displayGrades() {
+    clearScreen();
+    printf("姓名\t學號\t數學\t英文\t物理\t平均\n");
+    for (int i = 0; i < student_count; i++) {
+        printf("%s\t%s\t", students[i].name, students[i].id);
+        for (int j = 0; j < SUBJECTS; j++) {
+            printf("%d\t", students[i].scores[j]);
+        }
+        printf("%.2f\n", students[i].average);
+    }
+    system("pause");
+}
+
+
 int main(){
 	printf("E1B31\n");
 	printf("E1B31\n");
